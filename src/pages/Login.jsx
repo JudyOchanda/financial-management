477c1171d20beb1f0c7ac5671264f88bf0cafeef
JudyOchanda <judyochanda686@gmail.com>
2024-01-31 12:28:37 +0300

    start login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 function Login() {
     return ( 
         <>
-         
+         <div className="container d-flex align-items-center">
+            
+         </div>
         </>
      );
 }
