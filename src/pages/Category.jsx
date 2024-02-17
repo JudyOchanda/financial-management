80173db0bd0f96d35a80a719200f92d126d6292a
dalienst <oduordalienst@gmail.com>
2024-02-17 13:49:39 +0300

    complete category screen

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React from "react";
+import Button from "react-bootstrap/Button";
+import { myExpenses } from "../data/expenses";
+import { Link } from "react-router-dom";
+import { category } from "../data/categorySample";
 
 function Category() {
   return (
-    <div>Category</div>
-  )
+    <div className="container py-2">
+      <section>
+        <div className="d-flex justify-content-between">
+          <h2 className="fw-bold text-uppercase">Category</h2>
+          <Button variant="outline-primary">New Category</Button>
+        </div>
+      </section>
+      <hr />
+
+      <section>
+        <div className="table-responsive">
+          <table className="table table-bordered">
+            <thead>
+              <tr>
+                <th>Color</th>
+                <th>Name</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {category.map((item) => (
+                <>
+                  <tr key={item.id}>
+                    <td>
+                      <button
+                        className="btn"
+                        style={{
+                          height: "20px",
+                          width: "20px",
+                          borderRadius: "50%",
+                          backgroundColor: item.color,
+                        }}
+                      ></button>
+                    </td>
+                    <td>{item.name}</td>
+                    <td>
+                      <Button variant="outline-primary">Manage</Button>
+                    </td>
+                  </tr>
+                </>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </section>
+    </div>
+  );
 }
 
-export default Category
+export default Category;
